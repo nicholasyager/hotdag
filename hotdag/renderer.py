f8c73cbd34f4632f21f8fb58ddd69e5a5ee79ef6
Nicholas Yager <yager@nicholasyager.com>
2023-07-18 22:08:14 -0400

    feat: Add GML renderer for wider compatibility with Graphing tools

--- hotdag/renderer.py
+++ hotdag/renderer.py
@@ -1,6 +1,8 @@
+import io
 import json
 from typing import Set
 
+import networkx
 from dbt.contracts.graph.manifest import Manifest
 from dbt.graph import Graph, UniqueId
 
@@ -38,11 +40,38 @@ class TextRenderer(Renderer):
         return "\n".join(selected_nodes)
 
 
+class GMLRenderer(Renderer):
+    """Render the selected graph as a GML Graph payload."""
+
+    def __init__(self):
+        super().__init__(content_type="application/gml")
+
+    def render(
+        self, manifest: Manifest, graph: Graph, selected_nodes: Set[UniqueId]
+    ) -> str:
+        """Render the selection as a JSON array of node UniqueIDs."""
+        subgraph = graph.get_subset_graph(selected_nodes)
+        output = io.BytesIO()
+        networkx.write_gml(subgraph.graph, path=output)
+        output.seek(0)
+        return output.read().decode("ascii")
+
+
 class SVGRenderer(Renderer):
     """Render the selection as an SVG of the subgraph."""
 
     def __init__(self):
         super().__init__(content_type="image/svg+xml")
+
+        self.color_map = {
+            "source": "#3B4F4C",
+            "seed": "#69855B",
+            "snapshot": "#97A37A",
+            "model": "#6C3A2B",
+            "metric": "#8B5320",
+            "test": "#C4293E",
+            "exposure": "#CEA248",
+        }
 
     def render(
         self, manifest: Manifest, graph: Graph, selected_nodes: Set[UniqueId]
@@ -52,7 +81,24 @@ class SVGRenderer(Renderer):
         subgraph = graph.get_subset_graph(selected_nodes).graph
         dot_code = nx.nx_pydot.to_pydot(subgraph)
         dot_code.set_rankdir("LR")
+
+        # Style the graphviz graph
+        for node in dot_code.get_nodes():
+            node.set_shape("box")
+            node_name = node.get_name()
+            resource_type = node_name.split(".")[0][1:]
+            node.set_color(self.color_map.get(resource_type, "black"))
+            node.set_fillcolor(self.color_map.get(resource_type, "black"))
+            node.set_fontcolor("white")
+            node.set_style("filled")
+            node.set_fontname("Arial")
+
         return dot_code.create_svg().decode("utf-8")
 
 
-renderers = {"json": JSONRenderer, "text": TextRenderer, "svg": SVGRenderer}
+renderers = {
+    "json": JSONRenderer,
+    "text": TextRenderer,
+    "svg": SVGRenderer,
+    "gml": GMLRenderer,
+}
